dce658f75c5121571d958e407fb5a4f827969c32
github romamahi41 <jack2r6yaba@gmail.com>
2023-11-08 22:37:23 +0900

    1108

---
+++ database.java
@@ -0,0 +1,13 @@
+//javaでSQLを呼び出す処理をまとめたクラス
+//作成者：山本
+//作成日：2019/7/9
+//最終更新日：2019/7/9
+//更新者：山本
+
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
